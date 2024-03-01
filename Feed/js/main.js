b29d149f39bd2472ce5f02bb733f724d2660d72f
Augusto Marques <augustomendesmarques@gmail.com>
2024-03-01 09:10:15 -0300

    First commit

---
+++ Feed/js/main.js
@@ -0,0 +1,61 @@
+class SlideStories {
+  constructor(id) {
+    this.slide = document.querySelector(`[data-slide="${id}"]`);
+    this.active = 0;
+    this.init();
+  }
+
+  activeSlide(index) {
+    this.active = index;
+    this.items.forEach((item) => item.classList.remove("active"));
+    this.items[index].classList.add("active");
+    this.thumbItems.forEach((item) => item.classList.remove("active"));
+    this.thumbItems[index].classList.add("active");
+    this.autoSlide();
+  }
+
+  autoSlide() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(this.next, 5000);
+  }
+
+  next() {
+    if (this.active > 0) {
+      this.activeSlide(this.active - 1);
+    } else {
+      this.activeSlide(this.items.length - 1);
+    }
+  }
+
+  prev() {
+    if (this.active < this.items.length - 1) {
+      this.activeSlide(this.active + 1);
+    } else {
+      this.activeSlide(0);
+    }
+  }
+
+  addNavigation() {
+    const nextBtn = this.slide.querySelector(".slide-next");
+    const prevBtn = this.slide.querySelector(".slide-prev");
+    nextBtn.addEventListener("click", this.next);
+    prevBtn.addEventListener("click", this.prev);
+  }
+
+  addThumbItems() {
+    this.items.forEach(() => (this.thumb.innerHTML += `<span></span>`));
+    this.thumbItems = Array.form(this.thumb.children);
+  }
+
+  init() {
+    this.next = this.next.blind(this);
+    this.prev = this.prev.blind(this);
+    this.items = this.slide.querySelectorAll(".slide-items > *");
+    this.thumb = this.slide.querySelector(".slide-thumb");
+    this.addThumbItems();
+    this.activeSlide(0);
+    this.addNavigation();
+  }
+}
+
+new SlideStories("slide");
